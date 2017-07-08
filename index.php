a82af675f90b19a0e4cd307b088c2329f77ee6f9
Denissimo <droden@narod.ru>
2017-07-08 19:17:06 +0300

    loadfromPDO

--- index.php
+++ index.php
@@ -1,5 +1,42 @@
 <?php
 
 use Doctrine\ORM;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 //$em = new ORM\EntityManager();
+
+require_once __DIR__ . "/vendor/autoload.php";
+$isDevMode = true;
+// Настройки будут браться из аннотаций, на мой взгляд, это удобнее
+// Заметьте, что здесь я передаю путь до каталога, который будет содержать в себе классы сущностей, проецируемые на БД
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/lib/Doctrine/"), $isDevMode);
+
+$parameters = array(
+
+);//Здесь вам надо вытащить настройки подключения к БД для вашего проекта
+//Здесь вам надо вытащить настройки подключения к БД для вашего проекта
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'host'     => 'localhost',
+    'user'     => 'root',
+    'password' => '',
+    'dbname'   => 'newyear',
+    'charset'  => 'UTF8',
+);
+$entityManager = EntityManager::create($dbParams, $config)->getConnection();
+//$entityManager->getConnection()->getDatabasePlatform();
+
+//$q = $entityManager->createQuery("SELECT * FROM sys_urls");
+$q = $entityManager->prepare("SELECT * FROM sys_urls");
+$q->execute();
+$r = $q->fetchAll();
+//$p = $q->bindParam();
+//$r = $q->getArrayResult();
+//$r = $q->getResult($q);
+//echo "ZZZZZZZZZ";
+echo "<pre>";
+var_dump($r);
+echo "</pre>";
+//print_r($q);
